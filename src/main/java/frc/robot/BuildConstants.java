32cd4be221a0360751127ba484c91e5770a484d5
Curt <clrozeboom@gmail.com>
2026-01-10 11:30:05 -0500

    Update dependencies and project for 2026 season
    Upgraded vendordep files and build.gradle to use 2026 non-beta versions, updated .wpilib/wpilib_preferences.json and license for 2026, and made minor .gitignore and code cleanups. Also added new simgui configuration entries and improved backup handling in the build process.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC5010Example";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 62;
-  public static final String GIT_SHA = "69177a924d9ea5f7ed2a2b5f0385831223fac2c1";
-  public static final String GIT_DATE = "2026-01-08 21:22:25 EST";
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "5793fc65d4ddb3e2c77f28797052b21386f6ed12";
+  public static final String GIT_DATE = "2026-01-10 10:18:27 EST";
   public static final String GIT_BRANCH = "beta";
-  public static final String BUILD_DATE = "2026-01-10 10:15:39 EST";
-  public static final long BUILD_UNIX_TIME = 1768058139857L;
+  public static final String BUILD_DATE = "2026-01-10 11:28:29 EST";
+  public static final long BUILD_UNIX_TIME = 1768062509191L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

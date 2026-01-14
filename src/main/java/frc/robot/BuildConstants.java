2ae1dc36c0ff359ddd00d6ad1037a954c455e91b
Adamtheboss9 <adamtheboss78@gmail.com>
2026-01-14 17:56:44 -0500

    Refactor subsystem configs and update build info
    Added climb.json and updated indexer.json, intake.json, and launcher.json to use a unified 'devices' structure and enable display. Moved intakeConfig files to intake directory. Renamed motor name in launcher/hood.json. Updated BuildConstants and cleaned up imports in Rebuilt.java.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 82;
-  public static final String GIT_SHA = "bbfe8d9ea52971e4d3b79d1a2cb5cba1548ac144";
-  public static final String GIT_DATE = "2026-01-14 17:19:14 EST";
+  public static final int GIT_REVISION = 89;
+  public static final String GIT_SHA = "f0ac91a814be066acdaaf157fdd11a7e720ed2c9";
+  public static final String GIT_DATE = "2026-01-14 17:46:10 EST";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-01-14 17:38:05 EST";
-  public static final long BUILD_UNIX_TIME = 1768430285886L;
+  public static final String BUILD_DATE = "2026-01-14 17:55:27 EST";
+  public static final long BUILD_UNIX_TIME = 1768431327493L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

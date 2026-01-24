b313a93a3231f36d1ae4cf6bd46d774ef5b34341
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-01-24 14:58:12 -0500

    Update Intake imports

--- src/main/java/frc/robot/rebuilt/Constants.java
+++ src/main/java/frc/robot/rebuilt/Constants.java
@@ -2,8 +2,8 @@ package frc.robot.rebuilt;
 
 import frc.robot.rebuilt.subsystems.Climb.Climb;
 import frc.robot.rebuilt.subsystems.Indexer;
-import frc.robot.rebuilt.subsystems.Intake;
 import frc.robot.rebuilt.subsystems.Launcher.Launcher;
+import frc.robot.rebuilt.subsystems.intake.Intake;
 
 public class Constants {
   public static final String INDEXER = Indexer.class.getSimpleName();

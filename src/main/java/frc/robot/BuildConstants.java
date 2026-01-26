b68c5dbe82a0feeb0e3a780e2e4d6048489e3852
Curt <clrozeboom@gmail.com>
2026-01-26 16:55:56 -0500

    Update camera configs and motor positions for rebuilt robot
    Replaced 'intake' and 'localization' cameras with new 'left' and 'right' camera configs using PhotonVision, and updated cameras.json accordingly. Added detailed 'robotToMotor' position data to climb, intake winch, launcher hood, and shooter motors. Adjusted position values for indexer feeder, spindexer, and intake spintake subsystems to reflect updated measurements.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 178;
-  public static final String GIT_SHA = "6adfb4d95fd4491eb02e5cbad2429d7dea6061a2";
-  public static final String GIT_DATE = "2026-01-26 13:16:35 EST";
+  public static final int GIT_REVISION = 179;
+  public static final String GIT_SHA = "c45401a66c1b69d5c554ae98661963ad4bb7520c";
+  public static final String GIT_DATE = "2026-01-26 13:22:35 EST";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-01-26 13:20:52 EST";
-  public static final long BUILD_UNIX_TIME = 1769451652832L;
+  public static final String BUILD_DATE = "2026-01-26 16:46:43 EST";
+  public static final long BUILD_UNIX_TIME = 1769464003236L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

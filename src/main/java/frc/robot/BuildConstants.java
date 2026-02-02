b5a660c277c74083ecb757b568598836c04bb820
pkatzfey <Katzfpet000@hsestudents.org>
2026-02-02 18:33:06 -0500

    Indexer State Machine Additional Transitions
    Bump build constants (GIT_REVISION, GIT_SHA, GIT_DATE, BUILD_DATE, BUILD_UNIX_TIME) to reflect a new build.
    
    Refactor IndexerCommands to extract state-specific command factories (idleStateCommand, churnStateCommand, feedStateCommand) instead of inlining Commands.*. Add Controller import and a configureButtonBindings(Controller driver, Controller operator) method to wire button actions and state transitions (with a TODO to adjust inputs). Add shouldIdle/shouldChurn/shouldFeed helper commands and fix churnStateCommand naming.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 216;
-  public static final String GIT_SHA = "0127ea1ce486eaa501dff1ab7911089d37e1a83d";
-  public static final String GIT_DATE = "2026-02-02 17:29:22 EST";
+  public static final int GIT_REVISION = 219;
+  public static final String GIT_SHA = "698819a4d4a0194280ad08369ca92c385d3cbaf1";
+  public static final String GIT_DATE = "2026-02-02 18:07:16 EST";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-02-02 17:39:22 EST";
-  public static final long BUILD_UNIX_TIME = 1770071962789L;
+  public static final String BUILD_DATE = "2026-02-02 18:19:07 EST";
+  public static final long BUILD_UNIX_TIME = 1770074347602L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

bd780bf9a9ea411302ea502e5fab20f6be2c4adb
LHT-Light <longtr11209@gmail.com>
2026-02-16 12:05:07 -0500

    comments

--- src/main/java/frc/robot/rebuilt/subsystems/Indexer/IndexerIOReal.java
+++ src/main/java/frc/robot/rebuilt/subsystems/Indexer/IndexerIOReal.java
@@ -1,8 +1,8 @@
-package frc.robot.rebuilt.subsystems.Indexer;
+package frc.robot.rebuilt.subsystems.indexer;
 
 import java.util.Map;
 import org.frc5010.common.motors.function.PercentControlMotor;
-
+/** Implements the hardware Indexer IO */
 public class IndexerIOReal implements IndexerIO {
   protected Map<String, Object> devices;
   private PercentControlMotor Spindexer;
@@ -14,24 +14,24 @@ public class IndexerIOReal implements IndexerIO {
     TransferBack = (PercentControlMotor) devices.get("transfer_back");
     this.devices = devices;
   }
-
+/** Updates indexer input values with current motor speed*/
   @Override
   public void updateInputs(IndexerIOInputs inputs) {
     inputs.spindexerSpeed = Spindexer.get();
     inputs.transferFrontSpeed = TransferFront.get();
     inputs.transferBackSpeed = TransferBack.get();
   }
-
+/** Sets the spindexer motor speed */
   @Override
   public void runSpindexer(double speed) {
     Spindexer.set(speed);
   }
-
+/** Sets the front transfer motor speed*/
   @Override
   public void runTransferFront(double speed) {
     TransferFront.set(speed);
   }
-
+/** Sets the back transfer motor speed */
   @Override
   public void runTransferBack(double speed) {
     TransferBack.set(speed);

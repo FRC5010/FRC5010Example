1061ef3fb85f424eba17bfeadb709f819b441163
LHT-Light <longtr11209@gmail.com>
2026-02-27 16:59:29 -0500

    Update LauncherIO.java

--- src/main/java/frc/robot/rebuilt/subsystems/Launcher/LauncherIO.java
+++ src/main/java/frc/robot/rebuilt/subsystems/Launcher/LauncherIO.java
@@ -21,24 +21,24 @@ public interface LauncherIO {
 
   @AutoLog
   public static class LauncherIOInputs {
+    /** Initializes the requested and current launcher states to idle */
     public LauncherCommands.LauncherState stateRequested = LauncherCommands.LauncherState.IDLE;
     public LauncherCommands.LauncherState stateCurrent = LauncherCommands.LauncherState.IDLE;
-
+    /** Intializes the distance to the virtual target and desired flywheel speed to start at 0 and calculation validity to false */
     public boolean isValidCalculation = false;
     public Distance distanceToVirtualTarget = Meters.of(0.0);
     public AngularVelocity flyWheelSpeedDesired = RPM.of(0.0);
-
+    /** Intializes calculated and desired angles to 0 degrees and calculated flywheel speed to 0 RPS */
     public AngularVelocity flyWheelSpeedCalculated = RotationsPerSecond.of(0.0);
     public Angle hoodAngleCalculated = Degrees.of(0.0);
     public Angle turretAngleCalculated = Degrees.of(0.0);
-
     public Angle hoodAngleDesired = Degrees.of(0.0);
     public Angle turretAngleDesired = Degrees.of(0.0);
-
+    /** Initializes actual flywheel speed to 0 RPM and actual hood and turret angles to 0 degrees */
     public AngularVelocity flyWheelSpeedActual = RPM.of(0.0);
     public Angle hoodAngleActual = Degrees.of(0.0);
     public Angle turretAngleActual = Degrees.of(0.0);
-
+    /** Decides whether the flywheel speed and turret and hood angle have reached their goals */
     public boolean flyWheelSpeedAtGoal = false;
     public boolean hoodAngleAtGoal = false;
     public boolean turretAngleAtGoal = false;

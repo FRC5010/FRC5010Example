3b8b2c75fcdf7b7310f046ba73fc3cf2eba237c6
Adam K <73131727+Adam-Kolt@users.noreply.github.com>
2026-02-28 08:55:29 -0500

    Calib value gear ratio fixes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 335;
-  public static final String GIT_SHA = "ce5584d5fe93e36f79ead4f84fabc81b039bfb53";
-  public static final String GIT_DATE = "2026-02-27 23:03:45 EST";
+  public static final int GIT_REVISION = 336;
+  public static final String GIT_SHA = "40e3afb679fe063649bb314b0f692d14a721f368";
+  public static final String GIT_DATE = "2026-02-28 08:29:54 EST";
   public static final String GIT_BRANCH = "RevealDayLockIn";
-  public static final String BUILD_DATE = "2026-02-28 07:55:24 EST";
-  public static final long BUILD_UNIX_TIME = 1772283324485L;
+  public static final String BUILD_DATE = "2026-02-28 08:53:42 EST";
+  public static final long BUILD_UNIX_TIME = 1772286822105L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

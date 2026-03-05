e1671a40e3cc6212868677194fffd8f246edb242
Curt <clrozeboom@gmail.com>
2026-03-05 12:35:15 -0500

    Lower motor logLevel and update build metadata
    Reduce logging verbosity for several motor configs (hopper, flywheel, hood, turret) by changing logLevel from HIGH to LOW in deploy JSONs. Also update build metadata in BuildConstants (GIT_REVISION, GIT_SHA, GIT_DATE, GIT_BRANCH, BUILD_DATE, BUILD_UNIX_TIME) to reflect a new build.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 432;
-  public static final String GIT_SHA = "7e15f0a6b413218ebcd5ea93fd8b28af4c3cd66a";
-  public static final String GIT_DATE = "2026-03-04 18:02:58 EST";
-  public static final String GIT_BRANCH = "many-updates";
-  public static final String BUILD_DATE = "2026-03-04 20:52:28 EST";
-  public static final long BUILD_UNIX_TIME = 1772675548380L;
+  public static final int GIT_REVISION = 435;
+  public static final String GIT_SHA = "28b00e6c68be5acbb7a8cc7dc6d036330f36f162";
+  public static final String GIT_DATE = "2026-03-04 21:15:18 EST";
+  public static final String GIT_BRANCH = "Mishawaka";
+  public static final String BUILD_DATE = "2026-03-05 12:29:25 EST";
+  public static final long BUILD_UNIX_TIME = 1772731765397L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

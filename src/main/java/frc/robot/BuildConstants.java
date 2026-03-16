7391d02109316873fc752dd1979c45fb01277d0d
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-03-16 18:42:26 -0400

    Partial changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 476;
-  public static final String GIT_SHA = "c560430e2446e4d48e4cc064569ef0cefb46a263";
-  public static final String GIT_DATE = "2026-03-14 12:39:03 EDT";
+  public static final int GIT_REVISION = 494;
+  public static final String GIT_SHA = "d5e5a507104680a46b858244c6b45ffcb5ca0afe";
+  public static final String GIT_DATE = "2026-03-16 18:21:55 EDT";
   public static final String GIT_BRANCH = "Columbus-Integration";
-  public static final String BUILD_DATE = "2026-03-14 13:59:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1773511156539L;
+  public static final String BUILD_DATE = "2026-03-16 18:22:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1773699773104L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

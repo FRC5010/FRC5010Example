99a27a8fe25821c76f094701da16bca03cbdf4f9
Kyle Heaton <kyle.f.heaton@gmail.com>
2026-03-17 18:54:07 -0400

    Build

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 501;
-  public static final String GIT_SHA = "76572f3ea8acde9e1fbad6d9fba25c5783dcabef";
-  public static final String GIT_DATE = "2026-03-17 12:54:06 EDT";
+  public static final int GIT_REVISION = 502;
+  public static final String GIT_SHA = "4637fe9726e6c3dfe297143a7f5942f68cc02bb1";
+  public static final String GIT_DATE = "2026-03-17 18:49:04 EDT";
   public static final String GIT_BRANCH = "Fix-intake-slop";
-  public static final String BUILD_DATE = "2026-03-17 18:47:31 EDT";
-  public static final long BUILD_UNIX_TIME = 1773787651776L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-17 18:49:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1773787788916L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }

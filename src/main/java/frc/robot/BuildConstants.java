a96fe178e752e5aaf6a086660f0b8f5112368120
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-03-17 18:52:48 -0400

    Rezeroed Turret

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 494;
-  public static final String GIT_SHA = "d5e5a507104680a46b858244c6b45ffcb5ca0afe";
-  public static final String GIT_DATE = "2026-03-16 18:21:55 EDT";
+  public static final int GIT_REVISION = 500;
+  public static final String GIT_SHA = "7133380ef07de0efab15419f26c94f2d9e153638";
+  public static final String GIT_DATE = "2026-03-16 20:55:07 EDT";
   public static final String GIT_BRANCH = "Columbus-Integration";
-  public static final String BUILD_DATE = "2026-03-16 18:22:53 EDT";
-  public static final long BUILD_UNIX_TIME = 1773699773104L;
+  public static final String BUILD_DATE = "2026-03-17 18:43:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1773787414417L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

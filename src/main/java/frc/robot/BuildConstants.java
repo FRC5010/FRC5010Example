d215ef3161133b00964cf23b8952aa6cb08173b2
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-03-17 20:10:50 -0400

    Added Extra Datapoints in Calibration for Shuttle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 505;
-  public static final String GIT_SHA = "47143b2c56cacc44fb5b26eed97537d5228a321c";
-  public static final String GIT_DATE = "2026-03-17 18:56:39 EDT";
+  public static final int GIT_REVISION = 506;
+  public static final String GIT_SHA = "03f8ccdb5c9356fcb169595c0b8db83f9bf1da08";
+  public static final String GIT_DATE = "2026-03-17 19:17:50 EDT";
   public static final String GIT_BRANCH = "Fix-intake-slop";
-  public static final String BUILD_DATE = "2026-03-17 19:06:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1773788775587L;
+  public static final String BUILD_DATE = "2026-03-17 20:01:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1773792107989L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

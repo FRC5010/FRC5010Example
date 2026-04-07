b66dea393032029a49c23168c9e7f5c655048644
Adam K. <73131727+Adam-Kolt@users.noreply.github.com>
2026-04-07 12:12:06 -0400

    Turret always turrets when turretting is gud for turreting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 548;
-  public static final String GIT_SHA = "ad5cdef4da0c50a84d670bb16a12ebdfc57aa8b7";
-  public static final String GIT_DATE = "2026-04-06 10:04:17 EDT";
-  public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-04-06 12:11:19 EDT";
-  public static final long BUILD_UNIX_TIME = 1775491879394L;
+  public static final int GIT_REVISION = 555;
+  public static final String GIT_SHA = "e54b32ef1728c94d2d66e821e5abdd06f16af277";
+  public static final String GIT_DATE = "2026-04-06 18:35:34 EDT";
+  public static final String GIT_BRANCH = "improvement/always-aim-turret";
+  public static final String BUILD_DATE = "2026-04-07 12:07:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1775578040437L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
